e2c9e7de8573f7d56f001bd67726a75a409b82e9
phibng16 <bao.job.2020@gmail.com>
2021-06-08 20:25:13 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-87hdez
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-87hdez)
---
+++ index.js
@@ -0,0 +1,15 @@
+var me = 1;
+var you = 2;
+
+if (1 < 2) {
+  console.log('yay');
+}
+
+var userIsloggedin = true;
+
+if (userIsloggedin) {
+  console.log('logged in')
+} else {
+  console.log('logged out')
+}
+
